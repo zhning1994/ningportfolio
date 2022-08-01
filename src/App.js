d7079b5e90cc7cafda0c8819da12c8d229e9cf53
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-08-01 20:11:56 +0800

    add <Intro/> section to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Intro from './components/Intro/Intro';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
