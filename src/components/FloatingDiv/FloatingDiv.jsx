1fd96e185ea0f1be9e0451c0a84bcbb7e21f775e
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-08-02 13:14:43 +0800

    setup the floating card

--- src/components/FloatingDiv/FloatingDiv.jsx
+++ src/components/FloatingDiv/FloatingDiv.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './FloatingDiv.css';
 
-const FloatingDiv = () => {
-  return <div>FloatingDiv</div>;
+const FloatingDiv = ({ img, text1, text2 }) => {
+  return (
+    <div className="floatingdiv">
+      <img src={img} alt="intro" />
+      <span>
+        {text1} <br /> {text2}
+      </span>
+    </div>
+  );
 };
 
 export default FloatingDiv;
