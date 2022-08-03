18ded83051db1f6282cbb05c33fb721815da1aac
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-08-03 19:37:48 +0800

    add <Experience/> to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Intro from './components/Intro/Intro';
+import Experiences from './components/Experiences/Experiences';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <Experiences />
     </div>
   );
 }
